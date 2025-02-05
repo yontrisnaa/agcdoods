04e1c3dbc3b664a37ebec26f35c2622c1e803b6a
Yon Trisna Kurniawan <105153046+yontrisnaa@users.noreply.github.com>
2025-02-05 23:37:11 +0700

    Update foot.php

--- foot.php
+++ foot.php
@@ -5,9 +5,9 @@
             <p class="text-white mt-3">&copy; <?= date("Y"); ?> DoodStream. All Rights Reserved.</p>
         </div>
     </footer>
-     <!-- Histats.com  START  (aync)-->
+   <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4882776,4,0,0,0,00010000']);
+_Hasync.push(['Histats.start', '1,4920030,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -15,9 +15,8 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4882776&101" alt="" border="0"></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4920030&101" alt="" border="0"></a></noscript>
 <!-- Histats.com  END  -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <!--<script type='text/javascript' src='//pl25385471.profitablecpmrate.com/df/e1/93/dfe19394932f645d6166c88adbf93b91.js'></script>-->
     <script src="/sop.js"></script>
